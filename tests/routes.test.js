48064aa5b07727a2f00098f19f0c4ed766b294c9
Oleg Miniuk <oleg.miniuk@vebtech.by>
2020-03-13 15:01:05 +0300

    unit-tests: test for external api

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,3 +1,5 @@
+// internal and external
+
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../bin/www');
@@ -6,8 +8,20 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
-describe('/menu', () => {
+describe('external menu api', () => {
   it('it should get menu from server', done => {
+    chai.request('http://backend-challenge-pos.pepperhq.com')
+      .get('/menu.json')
+      .end((err, res) => {
+        expect([200, 304]).include(res.status);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+});
+
+describe('/menu', () => {
+  it('it should get extended menu from our api', done => {
     chai.request(server)
       .get('/menu')
       .end((err, res) => {
